59d032d01a8836d77900a89a07f86a0385f0a97b
vikkkrant <vikrantbiniwale@gmail.com>
2023-01-24 20:58:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events')
+
+class MyEmitter extends EventEmitter{};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the log event
+myEmitter.on('log',(msg)=> logEvents(msg));
+
+setTimeout(()=>{
+    myEmitter.emit('log','Log event emitted')
+})
---
+++ logEvents.js
@@ -0,0 +1,24 @@
+const { format } = require('date-fns');
+const { v4:uuid} = require('uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+const { log } = require('console');
+
+const logEvents = async (message) =>{
+    const dateTime = `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}`
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+    console.log(logItem);
+    try{
+        if(!fs.existsSync(path.join(__dirname,'logs'))){
+            await fsPromises.mkdir(path.join(__dirname,'logs'))
+        }
+
+        // testing
+        await fsPromises.appendFile(path.join(__dirname,'logs','eventsLog.txt'),logItem);
+    }catch(err){
+        console.log(err);
+    }
+}
+module.exports = logEvents;
